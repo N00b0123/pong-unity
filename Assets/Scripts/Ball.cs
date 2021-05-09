f4ddc4ab6480b6dccf40065f3129cbf9318457b5
William <williamgrosseli@gmail.com>
2021-05-09 18:16:40 -0300

    Score implementation

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,23 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
     public float Speed = 5f;
+    public GameObject left;
+    public GameObject rigth;
+    public Vector3 initialPosition =  new Vector3(0,0,0);
+    public float SpeedX;
+    public float SpeedY;
+    public int ScoreLeft = 0;
+    public int ScoreRigth = 0;
+    public Text RigthScore, LeftScore;
+   
     // Start is called before the first frame update
     void Start()
     {
-        float SpeedX = Random.Range(0,2) == 0 ? -1 : 1;
-        float SpeedY = Random. Range(0,2) == 0 ? -1 : 1;
+        RigthScore.text = ScoreRigth.ToString();
+        LeftScore.text = ScoreLeft.ToString();
 
-        GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0f);
-
+        float SpeedX = Random.Range(0, 2) == 0 ? -1 : 1;
+        float SpeedY = Random.Range(0, 2) == 0 ? -1 : 1;
+        GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider colision)
     {
-        
+        if(colision.gameObject.tag == "ScoreR")
+        {
+            ScoreLeft++;
+            LeftScore.text = ScoreLeft.ToString();
+            Reset();
+        }
+        if (colision.gameObject.tag == "ScoreL")
+        {
+            ScoreRigth++;
+            RigthScore.text = ScoreRigth.ToString();
+            Reset();
+        }
     }
+
+    void Reset()
+        {
+            gameObject.transform.position = initialPosition;
+            SpeedX *= -1;
+            SpeedY = Random.Range(0, 2) == 0 ? -1 : 1;
+            GetComponent<Rigidbody>().velocity = new Vector3(Speed * SpeedX, Speed * SpeedY, 0);
+        }
 }
