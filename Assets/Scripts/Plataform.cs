c58ba8da8d9972ac2752bdb3562843e9cafecfa2
William <williamgrosseli@gmail.com>
2021-05-09 17:14:51 -0300

    Pong-Unity
    Pong game in Unity

---
+++ Assets/Scripts/Plataform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataform : MonoBehaviour
+{
+    public float Speed = 5f;
+    public bool IsRigth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(IsRigth)
+            {
+                transform.Translate(0f, Input.GetAxis("VerticalR") * Speed * Time.deltaTime, 0f);
+            }
+        else
+            {
+                transform.Translate(0f, Input.GetAxis("VerticalL") * Speed * Time.deltaTime, 0f);
+            }
+        
+    }
+}
