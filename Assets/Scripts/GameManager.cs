360f9f5c9350dc07970648915043898f540493dc
William <williamgrosseli@gmail.com>
2021-10-13 21:46:33 -0300

    Menu update

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool isPaused = false;
+    public GameObject scoreUI;
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }    
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        scoreUI.SetActive(false);
+        pauseUI.SetActive(true);
+        isPaused = true;
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        pauseUI.SetActive(false);
+        scoreUI.SetActive(true);
+        isPaused = false;
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        isPaused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("EXIT GAME");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/Plataform.cs
+++ Assets/Scripts/Plataform.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Plataform : MonoBehaviour
 {
-    public float Speed = 5f;
+    [SerializeField] float Speed = 5f;
     public bool IsRigth;
 
-    // Update is called once per frame
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
+        MaxPosition();
+
         if(IsRigth)
-            {
                 transform.Translate(0f, Input.GetAxis("VerticalR") * Speed * Time.deltaTime, 0f);
-            }
         else
-            {
                 transform.Translate(0f, Input.GetAxis("VerticalL") * Speed * Time.deltaTime, 0f);
-            }
-        
+    }
+
+    void MaxPosition()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Mathf.Clamp(pos.y, -4, 6);
+        transform.position = pos;
     }
 }
